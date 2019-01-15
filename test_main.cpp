9635c9740370b221da10f435079543e2255e0e3f
Maximilian Maercz <m.maercz@gmail.com>
2019-01-15 12:07:39 +0100

    no global variables

--- test_main.cpp
+++ test_main.cpp
@@ -7,19 +7,25 @@
 #include<iterator>
 #include<set>
 
-extern std::vector<std::string> feature_names;
+/* extern std::vector<std::string> feature_names;
 extern std::vector<std::string> feature_types;
 extern std::vector<std::set<std::string>> feature_sets;
+extern std::vector<std::set<float>> numerical_sets;
 extern std::vector<std::string> num_feature_names;     //for saving in correct order
-extern std::vector<std::string> cat_feature_names;
+extern std::vector<std::string> cat_feature_names; */
 bool save_file = false;
 
 int main(){
   std::vector<Dataset> a;
-  a = load(true, "devtesting.dat");
+  a = load_Dataset_from_file(true, "devtesting.dat");
   set_feature_set(a);
+  set_numerical_set(a);
   // Test if data is correct:
 
+/* 
+  int x = 0;
+  std::cin >> x; 
+*/
   std::cout << a[44].num_features[0] << std::endl;
   // int b;
   // std::cin >> b;
@@ -28,7 +34,7 @@ int main(){
   std::cout << a.size() << std::endl;
   std::cout << a[46].cat_features.size() << std::endl;
   std::cout << a[46].num_features.size() << std::endl;
-  for ( auto j : feature_sets){
+  for ( auto j : Dataset::feature_sets){
     for (auto i : j)
       std::cout << i;
     std::cout << std::endl;
